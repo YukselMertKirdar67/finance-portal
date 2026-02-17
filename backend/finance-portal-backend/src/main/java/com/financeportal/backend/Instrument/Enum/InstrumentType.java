5fbc5a4d3ec2fec9764aeea0d16af42be5d9349b
YukselMertKirdar67 <yukselkirdar20@gmail.com>
2026-02-17 19:47:50 +0300

    Finansal enstrümanların tipleri oluşturuldu

---
+++ backend/finance-portal-backend/src/main/java/com/financeportal/backend/Instrument/Enum/InstrumentType.java
@@ -0,0 +1,10 @@
+package com.financeportal.backend.Instrument.Enum;
+
+public enum InstrumentType {
+    FOREX,        // Döviz (USD/TRY, EUR/USD)
+    STOCK,        // Borsa - Hisse Senetleri (AAPL, THYAO)
+    PRECIOUS,     // Değerli Madenler (GOLD, SILVER, XAU/USD)
+    CRYPTO,       // Kripto (BTC, ETH)
+    EUROBOND,     // EuroBond
+    BOND          // Tahvil/Bono
+}
