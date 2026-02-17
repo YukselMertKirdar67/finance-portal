2c83d6d72cf8ecd251f434467c20b4ae3f61c562
YukselMertKirdar67 <yukselkirdar20@gmail.com>
2026-02-17 19:49:18 +0300

    Kafka kullanılması için consumer oluşturuldu

---
+++ log/log-consumer/src/test/java/com/financeportal/logconsumer/LogConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financeportal.logconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
