2ccc151b416b4caeb49e1fa091b8357ab81518a5
YukselMertKirdar67 <yukselkirdar20@gmail.com>
2026-02-17 19:29:08 +0300

    Admin kullanıcısı ile ilgili service

---
+++ backend/finance-portal-backend/src/main/java/com/financeportal/backend/Service/AdminService.java
@@ -0,0 +1,12 @@
+/*package com.financeportal.backend.Service;
+
+import com.financeportal.backend.DTO.UserResponseDTO;
+
+import java.util.List;
+
+public interface AdminService {
+
+    List<UserResponseDTO> getAllUsers();
+
+    void disableUser(Long userId);
+}*/
