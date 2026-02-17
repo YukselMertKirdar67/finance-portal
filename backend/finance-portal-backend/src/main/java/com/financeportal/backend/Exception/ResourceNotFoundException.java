f20ee4747e73dc647fbedd727e468cfd6da12c7b
YukselMertKirdar67 <yukselkirdar20@gmail.com>
2026-02-17 19:58:17 +0300

    Hata classı oluşturuldu

---
+++ backend/finance-portal-backend/src/main/java/com/financeportal/backend/Exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.financeportal.backend.Exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
