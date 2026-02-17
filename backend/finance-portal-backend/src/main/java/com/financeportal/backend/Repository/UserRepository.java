8b353164bb6fa21b0394d2073484f7fbeadc70b0
YukselMertKirdar67 <yukselkirdar20@gmail.com>
2026-02-17 20:03:12 +0300

    Kullanıcılara databaseden erişmek için repository oluşturuldu

---
+++ backend/finance-portal-backend/src/main/java/com/financeportal/backend/Repository/UserRepository.java
@@ -0,0 +1,13 @@
+/*package com.financeportal.backend.Repository;
+
+import com.financeportal.backend.Entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByKeycloakId(String keycloakId);
+
+    boolean existsByKeycloakId(String keycloakId);
+}*/
