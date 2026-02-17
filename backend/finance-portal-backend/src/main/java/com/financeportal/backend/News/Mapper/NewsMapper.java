fa0486c3b4d3875d4b0aa31de270298c6af6c707
YukselMertKirdar67 <yukselkirdar20@gmail.com>
2026-02-17 19:51:47 +0300

    Haberler için mapper oluşturuldu

---
+++ backend/finance-portal-backend/src/main/java/com/financeportal/backend/News/Mapper/NewsMapper.java
@@ -0,0 +1,35 @@
+package com.financeportal.backend.News.Mapper;
+
+import com.financeportal.backend.News.DTO.NewsRequestDTO;
+import com.financeportal.backend.News.DTO.NewsResponseDTO;
+import com.financeportal.backend.News.Entity.News;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+public class NewsMapper {
+
+    public NewsResponseDTO toResponseDto(News news) {
+        return new NewsResponseDTO(
+                news.getId(),
+                news.getTitle(),
+                news.getContent(),
+                news.getSource(),
+                news.getCategory(),
+                news.getImageUrl(),
+                news.getPublishDate() != null ? news.getPublishDate() : LocalDateTime.now()
+        );
+    }
+
+    public News toEntity(NewsRequestDTO dto) {
+        News news = new News();
+        news.setTitle(dto.getTitle());
+        news.setContent(dto.getContent());
+        news.setSource(dto.getSource());
+        news.setCategory(dto.getCategory());
+        news.setPublishDate(LocalDateTime.now());
+        return news;
+    }
+}
+
