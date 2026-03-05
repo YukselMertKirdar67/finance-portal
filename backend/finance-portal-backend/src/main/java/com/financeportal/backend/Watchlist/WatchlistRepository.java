72598c2e9bee987c460e648c3604392c4415579a
YukselMertKirdar67 <yukselkirdar20@gmail.com>
2026-03-05 18:02:54 +0300

    Watchlist için repository oluşturuldu , içerisine id ile ilgili methodlar eklendi

---
+++ backend/finance-portal-backend/src/main/java/com/financeportal/backend/Watchlist/WatchlistRepository.java
@@ -0,0 +1,25 @@
+package com.financeportal.backend.Watchlist;
+
+import com.financeportal.backend.Instrument.Entity.BaseInstrument;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface WatchlistRepository extends JpaRepository<Watchlist, Long> {
+
+    List<Watchlist> findByUserIdOrderByAddedAtDesc(String userId);
+    Page<Watchlist> findByUserId(String userId, Pageable pageable);
+
+    Optional<Watchlist> findByUserIdAndInstrument(String userId, BaseInstrument instrument);
+
+    boolean existsByUserIdAndInstrument(String userId, BaseInstrument instrument);
+
+    void deleteByUserIdAndInstrument(String userId, BaseInstrument instrument);
+
+    long countByUserId(String userId);
+}
