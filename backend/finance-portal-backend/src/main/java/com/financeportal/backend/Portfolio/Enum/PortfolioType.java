78de1753e6c677922127f80317046bca0cbf1373
YukselMertKirdar67 <yukselkirdar20@gmail.com>
2026-03-08 13:51:43 +0300

    Portfolio için type tanımlaması eklendi

---
+++ backend/finance-portal-backend/src/main/java/com/financeportal/backend/Portfolio/Enum/PortfolioType.java
@@ -0,0 +1,8 @@
+package com.financeportal.backend.Portfolio.Enum;
+
+public enum PortfolioType {
+    PERSONAL,      // Bireysel portföy
+    BUSINESS,      // İşletme portföyü
+    RETIREMENT,    // Emeklilik portföyü
+    SAVINGS        // Tasarruf portföyü
+}
