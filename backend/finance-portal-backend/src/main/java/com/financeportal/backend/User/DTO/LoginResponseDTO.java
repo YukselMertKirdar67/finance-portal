bf68936b87a38593b4d4f00800c0ed37a381902f
YukselMertKirdar67 <yukselkirdar20@gmail.com>
2026-03-15 18:21:45 +0300

    Login için DTO oluşturuldu ve dönecek veriler tanımlandı

---
+++ backend/finance-portal-backend/src/main/java/com/financeportal/backend/User/DTO/LoginResponseDTO.java
@@ -0,0 +1,23 @@
+package com.financeportal.backend.User.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class LoginResponseDTO {
+
+    private boolean success;
+    private String message;
+    private String accessToken;
+    private String refreshToken;
+    private String username;
+    private String email;
+    private List<String> roles;
+}
