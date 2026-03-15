a1ffb53bdbcb301f5cf254043a947ed870b7012b
YukselMertKirdar67 <yukselkirdar20@gmail.com>
2026-03-15 09:52:48 +0300

    DTO oluşturuldu

---
+++ backend/finance-portal-backend/src/main/java/com/financeportal/backend/User/DTO/PasswordResetResponseDTO.java
@@ -0,0 +1,16 @@
+package com.financeportal.backend.User.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PasswordResetResponseDTO {
+
+    private boolean success;
+    private String message;
+}
