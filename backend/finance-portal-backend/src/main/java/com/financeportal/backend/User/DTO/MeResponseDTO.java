b2620af81fd758e9e2c47aca8fb3371d38b3f942
YukselMertKirdar67 <yukselkirdar20@gmail.com>
2026-03-15 18:22:08 +0300

    Long id -> String id değiştirildi

--- backend/finance-portal-backend/src/main/java/com/financeportal/backend/User/DTO/MeResponseDTO.java
+++ backend/finance-portal-backend/src/main/java/com/financeportal/backend/User/DTO/MeResponseDTO.java
@@ -11,7 +11,7 @@ import java.util.List;
 @AllArgsConstructor
 public class MeResponseDTO {
 
-    private Long id;
+    private String id;
     private String username;
     private String email;
     private List<String> roles;
