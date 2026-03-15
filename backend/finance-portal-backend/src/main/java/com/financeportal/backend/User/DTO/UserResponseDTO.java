09b5071d57c93e35e0bf70ff09024d1f1905236f
YukselMertKirdar67 <yukselkirdar20@gmail.com>
2026-03-15 18:30:05 +0300

    Long id -> String id değiştirildi ve emailVerified , createdAt eklendi

--- backend/finance-portal-backend/src/main/java/com/financeportal/backend/User/DTO/UserResponseDTO.java
+++ backend/finance-portal-backend/src/main/java/com/financeportal/backend/User/DTO/UserResponseDTO.java
@@ -1,16 +1,22 @@
 package com.financeportal.backend.User.DTO;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class UserResponseDTO {
 
-    private Long id;
+    private String id;
     private String username;
     private String email;
     private boolean enabled;
+    private boolean emailVerified;
+    private LocalDateTime createdAt;
 }
