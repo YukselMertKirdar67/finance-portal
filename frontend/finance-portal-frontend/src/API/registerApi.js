03771bb138af089057094767191e6c84b170d853
YukselMertKirdar67 <yukselkirdar20@gmail.com>
2026-03-15 18:26:52 +0300

    Register için backend entegrasyonu sağlandı

---
+++ frontend/finance-portal-frontend/src/API/registerApi.js
@@ -0,0 +1,12 @@
+import api from './instrumentsApi';
+
+
+export const registerUser = async (userData) => {
+    try {
+        const response = await api.post('/auth/register', userData);
+        return response.data;
+    } catch (error) {
+        console.error('Error registering user:', error);
+        throw error;
+    }
+};
