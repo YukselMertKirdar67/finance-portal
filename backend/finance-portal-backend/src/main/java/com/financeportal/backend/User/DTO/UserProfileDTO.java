fb4a9d870eba9c91a6ef0af7b28a68d486d761bf
YukselMertKirdar67 <yukselkirdar20@gmail.com>
2026-03-16 20:53:52 +0300

    User Profil için DTO oluşturuldu

---
+++ backend/finance-portal-backend/src/main/java/com/financeportal/backend/User/DTO/UserProfileDTO.java
@@ -0,0 +1,23 @@
+package com.financeportal.backend.User.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserProfileDTO {
+
+    private String id;  // Keycloak UUID
+    private String username;
+    private String email;
+    private Boolean emailVerified;
+    private List<String> roles;
+    private LocalDateTime createdAt;
+}
