7b89aa21c8720f0a7e239f7fa9d1dce8289fda4d
YukselMertKirdar67 <yukselkirdar20@gmail.com>
2026-03-22 15:29:48 +0300

    exitsByUsername, existByEmail eklendi

--- backend/finance-portal-backend/src/main/java/com/financeportal/backend/User/Repository/UserRepository.java
+++ backend/finance-portal-backend/src/main/java/com/financeportal/backend/User/Repository/UserRepository.java
@@ -8,6 +8,10 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByKeycloakId(String keycloakId);
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
 
     boolean existsByKeycloakId(String keycloakId);
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
 }
