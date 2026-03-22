dc8591782abba295e37705cc6859b7b968ba6844
YukselMertKirdar67 <yukselkirdar20@gmail.com>
2026-03-22 15:30:20 +0300

    updateUsername, updateEmail ve getPasswordLastChanged tanımlandı

--- backend/finance-portal-backend/src/main/java/com/financeportal/backend/User/Service/UserService.java
+++ backend/finance-portal-backend/src/main/java/com/financeportal/backend/User/Service/UserService.java
@@ -3,10 +3,16 @@ package com.financeportal.backend.User.Service;
 import com.financeportal.backend.User.Entity.User;
 import org.springframework.security.oauth2.jwt.Jwt;
 
+import java.time.LocalDateTime;
+
 public interface UserService {
 
 
     User getOrCreateUser(Jwt jwt);
 
     User getByKeycloakId(String keycloakId);
+
+    void updateUsername(String userId, String newUsername);
+    void updateEmail(String userId, String newEmail, String password);
+    LocalDateTime getPasswordLastChanged(String userId);
 }
