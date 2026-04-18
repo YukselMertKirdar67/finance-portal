c0538acc522b81c7902fc0791dd4e2b47037a9ef
YukselMertKirdar67 <yukselkirdar20@gmail.com>
2026-04-18 17:26:10 +0300

    Türkçe karakterler için converter eklendi

--- backend/finance-portal-backend/src/main/java/com/financeportal/backend/Config/RestTemplateConfig.java
+++ backend/finance-portal-backend/src/main/java/com/financeportal/backend/Config/RestTemplateConfig.java
@@ -3,7 +3,10 @@ package com.financeportal.backend.Config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
+
+import java.nio.charset.StandardCharsets;
 
 @Configuration
 public class RestTemplateConfig {
@@ -11,6 +14,9 @@ public class RestTemplateConfig {
     @Bean
     public RestTemplate restTemplate() {
         RestTemplate restTemplate = new RestTemplate();
+
+        restTemplate.getMessageConverters()
+                .add(0, new StringHttpMessageConverter(StandardCharsets.UTF_8));
 
         // Timeout ayarları
         SimpleClientHttpRequestFactory factory =
