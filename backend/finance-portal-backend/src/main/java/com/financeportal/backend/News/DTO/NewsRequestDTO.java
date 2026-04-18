5be83dba918fb63fcfdb87672af4b68518fc7428
YukselMertKirdar67 <yukselkirdar20@gmail.com>
2026-04-18 17:25:26 +0300

    imageUrl eklendi

--- backend/finance-portal-backend/src/main/java/com/financeportal/backend/News/DTO/NewsRequestDTO.java
+++ backend/finance-portal-backend/src/main/java/com/financeportal/backend/News/DTO/NewsRequestDTO.java
@@ -3,15 +3,16 @@ package com.financeportal.backend.News.DTO;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
-@Getter
-public class NewsRequestDTO {
-    @NotBlank
-    private String title;
+    @Getter
+    public class NewsRequestDTO {
+        @NotBlank
+        private String title;
 
-    @NotBlank
-    private String content;
+        @NotBlank
+        private String content;
 
-    private String source;
-    private String category;
+        private String source;
+        private String category;
+        private String imageUrl;
 
-}
+    }
