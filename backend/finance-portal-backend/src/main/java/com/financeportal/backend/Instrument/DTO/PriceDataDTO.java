88355e54a4d78aadbaf5214cf21dead8e8b71dc3
YukselMertKirdar67 <yukselkirdar20@gmail.com>
2026-04-19 18:51:33 +0300

    currency ve exchangeRate eklendi

--- backend/finance-portal-backend/src/main/java/com/financeportal/backend/Instrument/DTO/PriceDataDTO.java
+++ backend/finance-portal-backend/src/main/java/com/financeportal/backend/Instrument/DTO/PriceDataDTO.java
@@ -2,6 +2,7 @@ package com.financeportal.backend.Instrument.DTO;
 
 import lombok.*;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
@@ -9,8 +10,10 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class PriceDataDTO {
+public class PriceDataDTO implements Serializable {
     private BigDecimal current;
+    private String currency; // Orijinal para birimi
+    private BigDecimal exchangeRate; // Kullanılan kur
     private BigDecimal open;
     private BigDecimal high;
     private BigDecimal low;
