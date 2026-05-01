686af2418fe9f760a67e10118a74ce6a7a85bfd2
YukselMertKirdar67 <yukselkirdar20@gmail.com>
2026-05-01 12:16:18 +0300

    Notifications için Enum oluşturuldu ve typelar tanımlandı

---
+++ backend/finance-portal-backend/src/main/java/com/financeportal/backend/Notification/NotificationType.java
@@ -0,0 +1,8 @@
+package com.financeportal.backend.Notification;
+
+public enum NotificationType {
+    PORTFOLIO_CHANGE,   // Portföy değer değişimi
+    PRICE_ALERT,        // Fiyat alarmı
+    TRANSACTION,        // İşlem bildirimi
+    NEWS                // Haber bildirimi
+}
