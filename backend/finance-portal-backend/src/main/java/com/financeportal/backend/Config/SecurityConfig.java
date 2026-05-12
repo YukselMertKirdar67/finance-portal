681b760c57cd249484cfb5c9e4295680375eb942
YukselMertKirdar67 <yukselkirdar20@gmail.com>
2026-05-12 12:51:13 +0300

    Swagger için requestMatchers eklendi

--- backend/finance-portal-backend/src/main/java/com/financeportal/backend/Config/SecurityConfig.java
+++ backend/finance-portal-backend/src/main/java/com/financeportal/backend/Config/SecurityConfig.java
@@ -39,7 +39,10 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         // Public endpoints (Swagger, Actuator)
                         .requestMatchers("/actuator/**").permitAll()
+                        .requestMatchers("/swagger-ui.html").permitAll()
                         .requestMatchers("/swagger-ui/**").permitAll()
+                        .requestMatchers("/v3/api-docs/**").permitAll()
+                        .requestMatchers("/v3/api-docs").permitAll()
                         .requestMatchers("/v3/api-docs/**").permitAll()
 
                         .requestMatchers("/api/auth/**").permitAll()
